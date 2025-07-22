45a622ead87f88deb87a646c10b1bf46a48c6f13
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2025-07-22 09:58:41 -0500

    broke on purpose to test gh wf

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
+	authHeader := headers.Get("Authorizationz")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
